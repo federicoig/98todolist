e439c66e0d508d853afb132f22a5fbbe530ffbfd
federico <federicoignacio1@gmail.com>
2020-09-21 19:02:18 -0300

    Updates

---
+++ precache-manifest.730e83d1a003bcdb31ce07699bebdaec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b674821a9f2b260b4fb8c97842901e",
+    "url": "/98todolist/index.html"
+  },
+  {
+    "revision": "fc992b9f9c623721b068",
+    "url": "/98todolist/static/css/2.1fe29dfa.chunk.css"
+  },
+  {
+    "revision": "cadb8538413fb223efe3",
+    "url": "/98todolist/static/css/main.36df44a4.chunk.css"
+  },
+  {
+    "revision": "fc992b9f9c623721b068",
+    "url": "/98todolist/static/js/2.c706980d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/98todolist/static/js/2.c706980d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cadb8538413fb223efe3",
+    "url": "/98todolist/static/js/main.d9277b76.chunk.js"
+  },
+  {
+    "revision": "91161947a1136ff66e55",
+    "url": "/98todolist/static/js/runtime-main.b73a09e1.js"
+  },
+  {
+    "revision": "1d4d404a3bbe4b7007f89e792233aa9c",
+    "url": "/98todolist/static/media/ms_sans_serif.1d4d404a.woff2"
+  },
+  {
+    "revision": "7bbbd32e0f9977dd70a4d1287927d5da",
+    "url": "/98todolist/static/media/ms_sans_serif.7bbbd32e.woff"
+  },
+  {
+    "revision": "6eb2c63fbead0014ef0c4c537bfe328a",
+    "url": "/98todolist/static/media/ms_sans_serif_bold.6eb2c63f.woff"
+  },
+  {
+    "revision": "cb1c7473de82bb53662aaa2d7640e53e",
+    "url": "/98todolist/static/media/ms_sans_serif_bold.cb1c7473.woff2"
+  }
+]);
